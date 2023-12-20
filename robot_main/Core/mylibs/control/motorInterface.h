b8bb7b538942c952c0db6662486a27f3bf7935df
JiangboWANG <zzdr2533@gmail.com>
2023-12-20 13:16:32 +0100

    update the motorinterface

--- robot_main/Core/mylibs/control/motorInterface.h
+++ robot_main/Core/mylibs/control/motorInterface.h
@@ -1,13 +1,5 @@
 #ifndef __MOTOR_INTERFACE_H__
 #define __MOTOR_INTERFACE_H__
 
-
-void carInit(void);
-void carLeft(void);
-void carRight(void);
-void carForward(void);
-void carBackward(void);
-void carStop(void);
-
-
+void driverMotor(uint16_t Vx, uint16_t Vz);
 #endif // __MOTOR_INTERFACE_H__
